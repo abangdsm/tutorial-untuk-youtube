e79bef4efccb3fb1c969fa6ac87563b62c7c9916
abangdsm <id.reversal@gmail.com>
2022-12-26 23:13:31 +0700

    Menambahkan tombol pada setiap parallax

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
 function gabung() {
     window.location.href = "https://tentangkode.com";
 }
+
+function github() {
+    window.location.href = "https://github.com/abangdsm";
+}
+
+function instagram() {
+    window.location.href = "https://instagram.com/dwistarmuda.id";
+}
